2536e60d8b23defe55e4bb61a333ea1c5bcf0b30
Chris Trześniewski <krzysztof.trzesniewski@scalac.io>
2020-11-10 22:29:25 +0100

    Fix environment.prod.ts

--- apps/gh-search/src/environments/environment.prod.ts
+++ apps/gh-search/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   clientId: '59e21db5eb01fd68ff0d',
-  appUrl: 'https://ghsearch-3e009.web.app/auth',
+  authRedirectUrl: 'https://ghsearch-3e009.web.app/auth',
   ghAuthorizeUrl: 'https://github.com/login/oauth/authorize'
 };
